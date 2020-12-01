3af73a8826f5b936f3de6c15111688219d1d5ea5
Akansha Kumari <aks9727kumari@gmail.com>
2020-12-01 13:13:09 +0530

    Update salary.cs

--- sa/codes/salary.cs
+++ sa/codes/salary.cs
@@ -186,21 +186,21 @@ namespace final
 
                 sqlCmd.Connection = sqlConn;
 
-                String ID = txtEmpID.Text;
+                String EmpID = txtEmpID.Text;
                 String Firstname = txtFirstname.Text;
                 String Surname = txtSurname.Text;
                 String Address = txtaddress.Text;
                 String Gender = combogender.Text;
                 String Mobile = txtmobile.Text;
                 String Cityweighting = txtcityweighting.Text;
-                String Salary= txtbasicsalary.Text;
-                String Overtime= txtovertime.Text;
+                String Basicsalary = txtbasicsalary.Text;
+                String Overtime = txtovertime.Text;
                 String Otherpayment = txtotherpayment.Text;
                 String Tax = txttax.Text;
                 String Pension = txtpension.Text;
                 String Studentloan = txtstudentloan.Text;
                 String NIPayment = txtNIPayment.Text;
-                String Paydate = dateTimePicker1.Text;
+                String Paydate1 = dateTimePicker1.Text;
                 String Taxperiod = txttaxperiod.Text;
                 String Taxcode = txttaxcode.Text;
                 String NIPeriod = txtNIPeriod.Text;
@@ -210,15 +210,17 @@ namespace final
                 String Netpay = txtnetpay.Text;
                 String Grosspay = txtgrosspay.Text;
                 String Deduction = txtdeduction.Text;
+                DateTime DateVal = DateTime.Now;
+                String Paydate = string.Format("{0:yyyy-MM-dd}", DateVal);
 
 
-                sqlCmd.CommandText = "update from employeedb set ID = '" + ID + "',Firstname = '" + Firstname + "',Surname = '" + Surname + "'," +
-                    "Address = '" + Address + "',Gender = '" + Gender + "',Mobile = '" + Mobile + "',Cityweighting = '" + Cityweighting + "',Salary = '" + Salary +
+                sqlCmd.CommandText = "UPDATE employeedb SET Firstname = '" + Firstname + "',Surname = '" + Surname + "'," +
+                    "Address = '" + Address + "',Gender = '" + Gender + "',Mobile = '" + Mobile + "',Cityweighting = '" + Cityweighting + "',Basicsalary = '" + Basicsalary +
                     "',Overtime = '" + Overtime + "', Otherpayment = '" + Otherpayment + "',Tax = '" + Tax + "',Pension = '" + Pension + "', Studentloan = '" + Studentloan +
                     "',NIPayment = '" + NIPayment + "',Paydate = '" + Paydate + "',Taxperiod = '" + Taxperiod + "',Taxcode = '" + Taxcode + "',NIPeriod = '" + NIPeriod +
                     "',NICode = '" + NICode + "',Taxablepay = '" + Taxablepay + "',Pensionablepay = '" + Pensionablepay + "',Netpay = '" + Netpay +
-                    "',Grosspay = '" + Grosspay + "',Deduction = '" + Deduction + "' WHERE ID = " + ID + "";
-
+                    "',Grosspay = '" + Grosspay + "',Deduction = '" + Deduction + "' WHERE EmpID = " + EmpID + "";
+                Console.WriteLine("PoojaPagal " + sqlCmd.CommandText);
 
 
 
@@ -232,7 +234,7 @@ namespace final
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message); 
+                MessageBox.Show(ex.Message);
             }
 
         }
@@ -243,10 +245,11 @@ namespace final
             {
 
                 sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database;
-                
+
                 sqlCmd.Connection = sqlConn;
-                String ID = txtEmpID.Text;
-                sqlCmd.CommandText = "delete * from employeedb where ID = "+ ID +"";
+                String EmpID = txtEmpID.Text;
+                sqlCmd.CommandText = "delete from employeedb where EmpID = " + EmpID + "";
+                Console.WriteLine("poojaMahaPagal" + sqlCmd.CommandText);
                 sqlConn.Open();
                 sqlRd = sqlCmd.ExecuteReader();
                 MessageBox.Show("Record deleted", "Employee System", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -257,7 +260,7 @@ namespace final
             {
                 MessageBox.Show(ex.Message, "Employee System", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-            
+
         }
 
         private void txtsearch_KeyPress(object sender, KeyPressEventArgs e)
@@ -266,6 +269,7 @@ namespace final
             {
                 if (e.KeyChar == (Char)13)
                 {
+                    refreshdb();
                     DataView dv = sqlDt.DefaultView;
                     dv.RowFilter = string.Format("Firstname like '%{0}%'", txtsearch.Text);
                     dataGridView1.DataSource = dv.ToTable();
@@ -281,6 +285,7 @@ namespace final
         {
             try
             {
+                refreshdb();
                 DataView dv = sqlDt.DefaultView;
                 dv.RowFilter = string.Format("Firstname like'%{0}%'", txtsearch.Text);
                 dataGridView1.DataSource = dv.ToTable();
@@ -294,59 +299,62 @@ namespace final
         private void btntotal_Click(object sender, EventArgs e)
         {
             try
-            { 
+            {
 
-            double Grosspay, Deduction, Netpay, Period;
-            double Intercity, Basicpay, Overtime, OtherPayment, Tax, Pension, StudentLoan, NIPayment;
+                double Grosspay, Deduction, Netpay, Period;
+                double Intercity, Basicpay, Overtime, OtherPayment, Tax, Pension, StudentLoan, NIPayment;
 
-            Intercity = Double.Parse(txtcityweighting.Text);
-            Basicpay = Double.Parse(txtbasicsalary.Text);
-            Overtime = Double.Parse(txtovertime.Text);
-            OtherPayment = Double.Parse(txtotherpayment.Text);
-            Grosspay = Intercity + Basicpay + Overtime + OtherPayment;
+                Intercity = Double.Parse(txtcityweighting.Text);
+                Basicpay = Double.Parse(txtbasicsalary.Text);
+                Overtime = Double.Parse(txtovertime.Text);
+                OtherPayment = Double.Parse(txtotherpayment.Text);
+                Grosspay = Intercity + Basicpay + Overtime + OtherPayment;
 
-            txtgrosspay.Text = String.Format("{0:c2}", Grosspay);
+                txtgrosspay.Text = String.Format("{0:c2}", Grosspay);
 
-            Tax = (Grosspay * 9) / 100;
-            Pension = (Grosspay * 12) / 100;
-            StudentLoan = (Grosspay * 5) / 100;
-            NIPayment = (Grosspay * 3) / 100;
+                Tax = (Grosspay * 9) / 100;
+                Pension = (Grosspay * 12) / 100;
+                StudentLoan = (Grosspay * 5) / 100;
+                NIPayment = (Grosspay * 3) / 100;
 
-            txttax.Text = String.Format("{0:c2}", Tax);
-            txtpension.Text = String.Format("{0:c2}", Pension);
-            txtstudentloan.Text = String.Format("{0:c2}", StudentLoan);
-            txtNIPayment.Text = String.Format("{0:c2}", NIPayment);
+                txttax.Text = String.Format("{0:c2}", Tax);
+                txtpension.Text = String.Format("{0:c2}", Pension);
+                txtstudentloan.Text = String.Format("{0:c2}", StudentLoan);
+                txtNIPayment.Text = String.Format("{0:c2}", NIPayment);
 
-            Deduction = Tax + Pension + StudentLoan + NIPayment;
-            txtdeduction.Text = String.Format("{0:c2}", Deduction);
+                Deduction = Tax + Pension + StudentLoan + NIPayment;
+                txtdeduction.Text = String.Format("{0:c2}", Deduction);
 
-            Netpay = Grosspay - Deduction;
-            txtnetpay.Text = String.Format("{0:c2}", Netpay);
-            
-            Period = Double.Parse(txttaxperiod.Text);
-            txttaxablepay.Text = String.Format("{0:c2}", Period);
-            txtpensionablepay.Text = String.Format("{0:c2}", Period * Pension);
-             // ==============================================================================================================================
-                    
-                
-                     sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database;
-                     sqlConn.Open();
-                     sqlCmd.Connection = sqlConn;
-                sqlCmd.CommandText = "insert into employeedb(ID,Firstname,Surname,Address,Gender," +
-               "Mobile,Weight,Salary,Overtime,Otherpay,Tax,Pension,Studentloan,NIPay" +
-               "Paydate,Taxperiod,Taxcode,NINumber,NICode,Taxable,Pensionable,Netpay,Grosspay,Deduction)" +
-               "values('"+ txtEmpID.Text + "','" + txtFirstname.Text + "','" + txtSurname.Text + "','" + txtaddress.Text + "'," +
+                Netpay = Grosspay - Deduction;
+                txtnetpay.Text = String.Format("{0:c2}", Netpay);
+
+                Period = Double.Parse(txttaxperiod.Text);
+                txttaxablepay.Text = String.Format("{0:c2}", Period);
+                txtpensionablepay.Text = String.Format("{0:c2}", Period * Pension);
+                // ==============================================================================================================================
+
+
+                sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database;
+                sqlConn.Open();
+                sqlCmd.Connection = sqlConn;
+                DateTime DateVal = DateTime.Now;
+                String dateInSqlFormat = string.Format("{0:yyyy-MM-dd}", DateVal);
+                Console.WriteLine("Pooja " + dateInSqlFormat);
+                sqlCmd.CommandText = "insert into employeedb(EmpID,Firstname,Surname,Address,Gender,Mobile,Cityweighting,"
+                    + "Basicsalary,Overtime,Otherpayment,Tax,Pension,Studentloan,NIPayment,Taxperiod,Taxcode,NIPeriod,NICode,"
+                    + "Taxablepay,Pensionablepay,Netpay,Grosspay,Deduction,Paydate)" +
+               "values('" + txtEmpID.Text + "','" + txtFirstname.Text + "','" + txtSurname.Text + "','" + txtaddress.Text + "'," +
                "'" + combogender.Text + "','" + txtmobile.Text + "','" + txtcityweighting.Text + "','" + txtbasicsalary.Text + "'," +
                "'" + txtovertime.Text + "','" + txtotherpayment.Text + "','" + txttax.Text + "','" + txtpension.Text + "','" + txtstudentloan.Text + "','" +
-               txtNIPayment.Text + "','" + dateTimePicker1.Text + "','" + txttaxperiod.Text + "','" + txttaxcode.Text + "','" + txtNIPeriod.Text + "','" +
+               txtNIPayment.Text + "','" + txttaxperiod.Text + "','" + txttaxcode.Text + "','" + txtNIPeriod.Text + "','" +
                txtNICode.Text + "','" + txttaxablepay.Text + "','" + txtpensionablepay.Text + "','" + txtnetpay.Text + "','" + txtgrosspay.Text + "','" +
-               txtdeduction.Text +"')";
-
+               txtdeduction.Text + "','" + dateInSqlFormat + "')";
+                Console.WriteLine("This is C# " + sqlCmd.CommandText);
                 sqlCmd.ExecuteNonQuery();
                 sqlConn.Close();
                 refreshdb();
 
-             // ==============================================================================================================================
+                // ==============================================================================================================================
             }
             catch (Exception ex)
             {
@@ -366,33 +374,33 @@ namespace final
         private void btnpayslip_Click(object sender, EventArgs e)
         {
             rtPayslip.Clear();
-            rtPayslip.AppendText("\t\t" + "Payslip" + "\t\t" +"\n");
-            rtPayslip.AppendText("\t\t" + "------------" + "\t\t" + "\n");
-            rtPayslip.AppendText("\t\t" + "EmpID" + "\t\t" + txtEmpID.Text +"\n");
-            rtPayslip.AppendText("\t\t" + "Firstname" + "\t\t" + txtFirstname.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Surname" + "\t\t" + txtSurname.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Payslip" + "\t\t" + "\n");
+            rtPayslip.AppendText("\t\t" + "----------------------------------------" + "\t\t" + "\n");
+            rtPayslip.AppendText("\t\t" + "EmpID" + "\t\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Firstname" + "\t" + txtFirstname.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Surname" + "\t" + txtSurname.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Address" + "\t\t" + txtaddress.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Gender" + "\t\t" + combogender.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Mobile" + "\t\t" + txtmobile.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Weight" + "\t\t" + txtcityweighting.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Cityweight" + "\t" + txtcityweighting.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Salary" + "\t\t" + txtbasicsalary.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Overtime" + "\t\t" + txtovertime.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Otherpayment" + "\t\t" + txtotherpayment.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Overtime" + "\t" + txtovertime.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Otherpayment" + "\t" + txtotherpayment.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Tax" + "\t\t" + txttax.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Pension" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Studentloan" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "NIPayment" + "\t\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Studentloan" + "\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "NIPayment" + "\t" + txtEmpID.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Paydate" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Taxperiod" + "\t\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Taxperiod" + "\t" + txtEmpID.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Taxcode" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "NIPeriod" + "\t\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "NIPeriod" + "\t" + txtEmpID.Text + "\n");
             rtPayslip.AppendText("\t\t" + "NICode" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Taxablepay" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Pensionablepay" + "\t\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Taxablepay" + "\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Pensionablepay" + "\t" + txtEmpID.Text + "\n");
             rtPayslip.AppendText("\t\t" + "Netpay" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Grosspay" + "\t\t" + txtEmpID.Text + "\n");
-            rtPayslip.AppendText("\t\t" + "Deduction" + "\t\t" + txtEmpID.Text + "\n");
-           
+            rtPayslip.AppendText("\t\t" + "Grosspay" + "\t" + txtEmpID.Text + "\n");
+            rtPayslip.AppendText("\t\t" + "Deduction" + "\t" + txtEmpID.Text + "\n");
+
 
         }
 
@@ -429,6 +437,47 @@ namespace final
             {
                 MessageBox.Show(ex.Message, "Employee System", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
+        }
+
+        private void btnsample_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtEmpID.Text = "501";
+                txtFirstname.Text = "Anita";
+                txtSurname.Text = "Maam";
+                txtaddress.Text = "Koramangala 5th Block";
+                txtmobile.Text = "1234567890";
+                txtcityweighting.Text = "90";
+                txtbasicsalary.Text = "150";
+                txtovertime.Text = "25";
+                txtotherpayment.Text = "75";
+                txttaxcode.Text = "TCIN008912";
+                txttaxperiod.Text = "3";
+                txtNIPeriod.Text = "TNIP008955";
+                txtNICode.Text = "TNCT008999";
+                combogender.Text = "Female";
+                txttax.Text = "";
+                txtpension.Text = "";
+                txtstudentloan.Text = "";
+                txtNIPayment.Text = "";
+                txttaxablepay.Text = "";
+                txtpensionablepay.Text = "";
+                txtnetpay.Text = "";
+                txtgrosspay.Text = "";
+                txtdeduction.Text = "";
+                txtsearch.Text = "";
+                dateTimePicker1.ResetText();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Employee System", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
     }
